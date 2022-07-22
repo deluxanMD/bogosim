36f9721cff6dd91c20d7f526e9794fa1195a4247
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2022-07-22 11:08:58 +0530

    search result design

--- src/components/selected-item/selected-item.jsx
+++ src/components/selected-item/selected-item.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import "./selected-item.css";
 
 const SelectedItem = ({ item }) => {
   return (
-    <div>
-      <p>{item.name}</p>
+    <div className="SelectedItem">
+      <h1>Search Result</h1>
+      <p>
+        <strong>Amount unit: </strong>
+        {item.amountUnit}
+      </p>
+      <p>
+        <strong>Amount value: </strong>
+        {item.amountValue}
+      </p>
+      <p>
+        <strong>PZN: </strong>
+        {item.pzn}
+      </p>
+      <p>
+        <strong>Name: </strong>
+        {item.name}
+      </p>
+      <p>
+        <strong>Stength unit: </strong>
+        {item.strengthUnit}
+      </p>
+      <p>
+        <strong>Stength value: </strong>
+        {item.strengthValue}
+      </p>
     </div>
   );
 };
