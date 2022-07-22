cd860402886d8ac778ccb4640bc3f83a33b71779
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2022-07-22 11:34:21 +0530

    typescript for search data component

--- src/pages/search-data.tsx
+++ src/pages/search-data.tsx
@@ -1,26 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, FormEvent } from "react";
 import "./search-data.css";
 import TextInput from "../components/text-input/text-input";
 import SearchResult from "../components/search-result/search-result";
 import SelectedItem from "../components/selected-item/selected-item";
+import { ItemI } from "../interface/item";
 
 const SearchData = () => {
-  const [keyword, setKeyword] = useState("");
-  const [temp, setTemp] = useState("");
-  const [selected, setSelected] = useState(null);
+  const [keyword, setKeyword] = useState<string>("");
+  const [temp, setTemp] = useState<string>("");
+  const [selected, setSelected] = useState<ItemI | null>(null);
 
-  const handleChange = (e) => {
-    setKeyword(e.target.value);
+  const handleChange = (e: FormEvent<HTMLInputElement>) => {
+    setKeyword((e.target as HTMLInputElement).value);
     setSelected(null);
   };
 
-  const handleSelect = (data) => {
+  const handleSelect = (data: ItemI) => {
     setSelected(data);
     setKeyword("");
     setTemp("");
   };
 
-  const handleHover = (data) => {
+  const handleHover = (data: ItemI) => {
     setTemp(data.name);
   };
 
@@ -30,7 +31,7 @@ const SearchData = () => {
         type="text"
         placeholder="Search data here"
         value={temp || keyword}
-        handleChange={(e) => handleChange(e)}
+        handleChange={(e: FormEvent<HTMLInputElement>) => handleChange(e)}
       />
       {keyword.length > 0 && !selected && (
         <SearchResult
