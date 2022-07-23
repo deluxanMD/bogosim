68061609b92f13596930b4d4e33480aa8641d1d9
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2022-07-23 21:24:59 +0530

    fuzzy search wth click handler

--- src/components/fuzzy-search/fuzzy-search.tsx
+++ src/components/fuzzy-search/fuzzy-search.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./fuzzy-search.css";
 import FuzzySearch from "react-fuzzy";
 import { ItemI } from "../../interface/item";
 import { RESULTS } from "../../mock-data/data";
@@ -7,15 +8,34 @@ interface IProps {
   getSelectedItem: (item: ItemI) => void;
 }
 
+interface IState {
+  isOpen: boolean;
+  results: ItemI[];
+  selectedIndex: number;
+  value: string;
+}
+
 const SearchFuzzy = (props: IProps) => {
   const { getSelectedItem } = props;
 
+  const handleSelect = (item: ItemI, state: IState) => {
+    getSelectedItem(item);
+    state.value = "";
+    state.results = [];
+  };
+
+  const handleMouseEnter = (item: ItemI, state: IState) => {
+    state.value = item.name;
+  };
+
   return (
     <FuzzySearch
+      autoFocus={true}
       list={RESULTS}
       keys={["name"]}
-      width={430}
-      onSelect={(item: any) => getSelectedItem(item)}
+      width={600}
+      maxResults={100}
+      onSelect={() => console.log("selected")}
       resultsTemplate={(props: any, state: any, styles: any) => {
         return state.results.map((val: any, i: number) => {
           const style =
@@ -23,7 +43,13 @@ const SearchFuzzy = (props: IProps) => {
               ? styles.selectedResultStyle
               : styles.resultsStyle;
           return (
-            <div key={i} style={style} onClick={() => getSelectedItem(val)}>
+            <div
+              key={i}
+              style={style}
+              onClick={() => handleSelect(val, state)}
+              onMouseEnter={() => handleMouseEnter(val, state)}
+              className="fuzzy-search-item"
+            >
               {val.name}
             </div>
           );
